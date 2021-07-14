df19a091c5fd82735a2acaf1c0a55183cfd9b2d8
Titan <crymmson@outlook.com>
2021-07-14 21:08:51 +1200

    inputbox ready

--- src/popup/LocationCard/LocationCard.tsx
+++ src/popup/LocationCard/LocationCard.tsx
@@ -1,17 +1,62 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from 'react';
+
+import {
+  Box,
+  IconButton,
+  Card,
+  CardContent,
+  Grid,
+  Typography
+} from '@material-ui/core';
 import { fetchTimeAndDistance } from '../../utils/api';
+import { Delete, Edit, LocationOn } from '@material-ui/icons';
 
 export const LocationCard: React.FC<{
   userLocation: string;
   listingLocation: string;
 }> = ({ userLocation, listingLocation }) => {
+  const [distance, setDistance] = useState<string | null>(null);
   useEffect(() => {
     fetchTimeAndDistance(userLocation, listingLocation)
       .then((data) => {
-        console.log(data.rows[0].elements[0].distance.text);
+        setDistance(data.rows[0].elements[0].distance.text);
       })
       .catch((error) => console.log(error));
-  }, [userLocation,listingLocation]);
-  return <div></div>;
+  }, [userLocation, listingLocation]);
+
+  if (!location) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Box mx='4px' my='8px'>
+      <Card>
+        <CardContent>
+          <Grid
+            container
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Grid item alignItems='center' xs={2}>
+              <LocationOn />
+            </Grid>
+            <Grid item alignItems='center' xs={6}>
+              <Typography variant='subtitle2'>{userLocation}</Typography>
+              <Typography variant='subtitle2'>({distance})</Typography>
+            </Grid>
+            <Grid item alignItems='center' xs={2}>
+              <IconButton>
+                <Edit color='primary' />
+              </IconButton>
+            </Grid>
+            <Grid item alignItems='center' xs={2}>
+              <IconButton>
+                <Delete color='secondary' />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 };
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './popup.css';
 import LocationCard from './LocationCard';
+import { Box, Grid, InputBase, IconButton, Paper } from '@material-ui/core';
+import { AddCircle as AddIcon } from '@material-ui/icons';
 
 const App: React.FC<{}> = () => {
+  const listingLocationRef = useRef('Auckland');
+  const [userLocations, setUserLocations] = useState<string[]>([
+    '230 Shirley Road, Papatoetoe, Auckland',
+    '230 Great South Road'
+  ]);
   return (
     <div>
-      <LocationCard userLocation='pipitea' listingLocation='porirua' />
+      <Box mx='4px' my='8px'>
+        <Paper>
+          <Box px='16px' py='4px'>
+            <Grid
+              container
+              direction='row'
+              justifyContent='center'
+              alignItems='center'
+            >
+              <Grid item xs={8}>
+                <InputBase placeholder='enter address here...' />
+              </Grid>
+              <Grid item xs={2}></Grid>
+              <Grid item alignItems='flex-end' xs={2}>
+                <IconButton>
+                  <AddIcon color='primary' />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Box>
+        </Paper>
+      </Box>
+      {userLocations.map((location, index) => (
+        <LocationCard
+          userLocation={location}
+          listingLocation={listingLocationRef.current}
+          key={index}
+        />
+      ))}
     </div>
   );
 };
