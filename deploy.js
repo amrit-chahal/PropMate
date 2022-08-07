d7aa26ca5296db59ee1ec6b2447c5334f1cd7ed1
Titan <crymmson@outlook.com>
2022-08-07 20:42:50 +1200

    new branch created

--- deploy.js
+++ deploy.js
@@ -5,3 +5,5 @@ const path = require('path');
 const chromeWebStore = require('chrome-webstore-manager');
 
 const extensionId = process.env.EXTENSION_ID;
+const buildPath = path.join(__dirname, "dist");
+console.log(buildPath);
