40e5f49219a78359c78ed32a93d24b4d710eb15d
Titan <crymmson@outlook.com>
2022-08-10 14:25:18 +1200

    refactor(deploy.js): deployment script updated to automate chrome webstore deployment

--- deploy.js
+++ deploy.js
@@ -2,8 +2,30 @@ const fs = require('fs-extra');
 const zipper = require('zip-local');
 const jsonFile = require('jsonfile');
 const path = require('path');
-const chromeWebStore = require('chrome-webstore-manager');
+const ChromeWebStore = require('chrome-webstore-manager');
+const { ChromeReaderMode } = require('@material-ui/icons');
 
 const extensionId = process.env.EXTENSION_ID;
-const buildPath = path.join(__dirname, "dist");
+const buildPath = path.join(__dirname, 'dist');
 console.log(buildPath);
+
+zipper.sync.zip(buildPath).compress().save(path.join(buildPath, 'dist.zip'));
+const fileBin = fs.readFileSync(path.join(buildPath, 'dist.zip'));
+
+const chromeWebStore = new ChromeWebStore(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET
+);
+
+chromeWebStore.getRefreshToken(process.env.REFRESH_TOKEN).then((data) => {
+  const json = JSON.parse(data);
+    const acessToken = json.access_token;
+    chromeWebStore.updateItem(accessToken, fileBin, process.env.EXTENSION_ID).then((data) => {
+        console.log(data)
+        chromeWebStore.publishItem(acessToken, process.env.EXTENSION_ID).then((data) => {
+            console.log(data)
+        })
+    })
+});
+
+console.log("extension deployed")
