5ccf63a1edda08a7f482e9a5c9e6456c93bce737
Titan <crymmson@outlook.com>
2022-08-22 14:46:19 +1200

    feat(webpack config and  manifest.json): webpack config changed and manfiest.json split into 2
    webpack config changed to output two different dist folders for chrome and firefox each.
    manifest.json split into two versions to support both chrome and firefox. manifest moved to their
    own respective folders for eoboth chrome and  firefox

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'cheap-module-source-map'
-});
+const configs = [];
+common.forEach((config) =>
+  configs.push(
+    merge(config, {
+      mode: 'development',
+      devtool: 'cheap-module-source-map'
+    })
+  )
+);
+module.exports = configs;
